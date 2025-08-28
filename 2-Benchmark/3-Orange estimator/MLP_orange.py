b9171f8bf5a4ea335cbfbc639a89f77dbe98ea3e
Nathan Doumèche <84311379+NathanDoumeche@users.noreply.github.com>
2025-08-28 18:36:03 +0100

    Add files via upload

---
+++ 2-Benchmark/3-Orange estimator/MLP_orange.py
@@ -0,0 +1,316 @@
+# packages import
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons # A simple synthetic dataset
+from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import mean_squared_error
+from scipy import stats
+
+from viking_kalman import expectation_maximization
+from viking_kalman import iterative_grid_search
+from viking_kalman import statespace
+from viking_kalman import expectation_maximization
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+
+# Device Configuration
+def get_device():
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+        print("Using MPS (Metal Performance Shaders) backend on Apple Silicon GPU.")
+    else:
+        device = torch.device("cpu")
+        print("MPS backend not available. Using CPU.")
+    return device
+
+device = get_device()
+
+
+
+####################################################################################################################################
+# Import Data
+####################################################################################################################################
+
+#National data
+Data = pd.read_csv('/Data/Outputs/dataset_national.csv')
+Data['date']= pd.to_datetime(Data['date'])
+Data.set_index("date", inplace=True)
+Data['Time']= np.arange(0, Data.shape[0])
+Data = pd.get_dummies(Data, columns=['day_type_week'], drop_first=True, dtype=int)
+Data = pd.get_dummies(Data, columns=['period_hour_changed'], drop_first=True, dtype=int)
+
+
+covariate_names = Data.columns.tolist()
+covariate_columns = ['Load', 'Load_d1', 'Load_d7', 'temperature',  'temperature_smooth_990',  'temperature_smooth_950', 'temperature_min_smooth_990',
+                     'temperature_max_smooth_990',
+                     'toy', 'day_type_week_1', 'day_type_week_2',
+                     'day_type_week_3', 'day_type_week_4', 'day_type_week_5', 'day_type_week_6', 'period_holiday', 'period_summer', 'period_christmas']
+
+
+Data = Data[covariate_columns]
+last_index = Data.index[-1]
+Data = Data.drop(last_index) 
+
+
+#MLP prev
+Data_prev = pd.read_csv('/Results/prediction_MLP_newdate_lag.txt')
+Data_prev = Data_prev.drop(columns=['Load'])
+Data_prev['date']= pd.to_datetime(Data_prev['date'])
+Data_prev.set_index("date", inplace=True)
+Data_prev.index
+
+#Orange Data
+Data_orange = pd.read_csv('/Data/data_orange.csv')
+Data_orange['date']= pd.to_datetime(Data_orange['date'])
+Data_orange = Data_orange[['date', 'Residents_aggl', 'Work', 'Exc_aggl', 'Tourists_aggl']]
+Data_orange.set_index("date", inplace=True)
+Data_orange.index
+Data_orange.columns.tolist()
+
+
+#merging
+merged_df = pd.merge(Data, Data_prev, left_index=True, right_index=True, how='inner')
+merged_df.columns.tolist()
+merged_df.index = merged_df.index.tz_localize(None)
+merged_orange = pd.merge(merged_df, Data_orange, left_index=True, right_index=True, how='inner')
+merged_orange.columns.tolist()
+merged_orange['Residuals'] = merged_orange['Load'] - merged_orange['NN']
+
+
+
+
+merged_orange.columns.tolist()
+merged_orange = merged_orange[covariate_columns]
+
+actual_all_hour = merged_orange.loc[:,'Residuals']
+prediction_all_hour_NN = actual_all_hour.copy()
+prediction_all_hour_NN_static = actual_all_hour.copy()
+prediction_all_hour_NN_dyn = actual_all_hour.copy()
+prediction_all_hour_NN_em = actual_all_hour.copy()
+
+
+
+for j in range(48):
+    hour = j // 2
+    minute = (j % 2) * 30
+    
+    start_time = f'{hour:02d}:{minute:02d}'
+    end_time = f'{hour:02d}:{minute+29:02d}'
+    
+    print('###############################################' + str(j))
+    print('###############################################' + str(start_time)+ '###########'+str(end_time))
+
+
+    Data_h = merged_orange.between_time(start_time, end_time)
+    
+    Data_h.index[0]
+    
+    Data_h.index.astype(str)[0] 
+    
+    
+
+    d1 = Data_h[Data_h.index.strftime('%Y-%m-%d %H:%M:%S') ==  "2022-09-01 "+ start_time + ":00"].index[0]
+    d2 = Data_h[Data_h.index.strftime('%Y-%m-%d %H:%M:%S') ==  "2022-09-02 "+ start_time + ":00"].index[0]
+         
+    mean_load_train = np.mean(Data_h.loc[:d1, 'Residuals'])
+    std_load_train = np.std(Data_h.loc[:d1,'Residuals'])
+    
+    mean_load_test = np.mean(Data_h.loc[d2:, 'Residuals'])
+    std_load_test = np.std(Data_h.loc[d2:,'Residuals'])
+    
+    Data_train = Data_h.loc[:d1, ]
+    Data_test = Data_h.loc[d2:, ]
+    
+    # choice of the scaling function
+    scaler = StandardScaler()
+    
+    # We convert data to torch tensors
+    variables_to_drop = ['Residuals', 'Load', 'NN', 'Static','Dyn','Em']
+    X0 = Data_train.drop(variables_to_drop, axis=1)
+ 
+    
+    X0 = scaler.fit_transform(X0)
+    X0 = X0.astype(np.float32)
+    X0_tensor = torch.from_numpy(X0)
+    
+    Y0 = Data_train[['Residuals']]
+    Y0 = scaler.fit_transform(Y0)
+    Y0 = Y0.astype(np.float32)
+    Y0_correct = Y0.reshape(-1, 1)
+    Y0_tensor = torch.from_numpy(Y0_correct)
+    
+    
+    class PytorchPerceptron(nn.Module):
+       def __init__(self, input_shape, hidden_shape, output_shape, activation=torch.nn.Tanh(), p=0):
+         #super().__init__()
+         super(PytorchPerceptron, self).__init__()
+         self.W1 = torch.nn.Linear(input_shape, hidden_shape)
+         self.activation = activation
+         self.dropout = nn.Dropout(p)
+         self.W2 = torch.nn.Linear(hidden_shape, output_shape)
+         ###
+         self.initialize_weights()
+         
+       def forward(self, x):
+         h = self.activation(self.W1(x))
+         h = self.dropout(h)
+         out = self.W2(h)
+         ###
+         return out
+     
+       def initialize_weights(self):
+         torch.nn.init.xavier_uniform_(self.W1.weight, gain=nn.init.calculate_gain('tanh'))
+         torch.nn.init.xavier_uniform_(self.W2.weight, gain=nn.init.calculate_gain('tanh'))
+    
+    
+    ###set the attributes to the right value given the considered dataset
+    input_shape = X0.shape[1]
+    hidden_shape = 100
+    output_shape = 1
+    ###
+    
+    nb_epochs = 30000
+    learning_rate = 0.001
+    batch_size = 1
+    
+    
+    model = PytorchPerceptron(input_shape, hidden_shape, output_shape).float()
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    
+    
+    l = np.arange(0,len(Y0)).tolist()
+    train_mask = np.random.choice(l, size=math.floor(len(Y0)*0.8), replace=False)
+    test_mask = np.setdiff1d(l, train_mask)
+    
+    train_step_loss = []
+    val_step_loss = []
+    
+    
+    def pytorch_training(model, x_data, y_data, nb_epochs, learning_rate, criterion, optimizer):
+      for epoch in range(0, nb_epochs):
+        ### TODO: implemente the training strategy using pytorch functions
+        # Forward pass
+        y_pred = model(x_data)
+        # Compute training Loss
+        loss = criterion(y_pred[train_mask], y_data[train_mask])
+        train_step_loss.append(loss.item())
+        # Get validation losses
+        validation_loss = criterion(y_pred[test_mask], y_data[test_mask])
+        val_step_loss.append(validation_loss.item())
+        # Zero gradient
+        optimizer.zero_grad()
+        # Back-propagation
+        loss.backward()
+        # One-step gradient
+        optimizer.step()
+        ###
+        if epoch % 1000 == 0:
+          print(f"Epoch {epoch} ===== loss {loss}")
+      return y_pred, model
+    
+    y_pred, model = pytorch_training(model, X0_tensor, Y0_tensor, nb_epochs, learning_rate, criterion, optimizer)
+
+    plt.figure(figsize=(10, 6)) # Set the figure size for better readability
+    plt.plot(np.arange(0,nb_epochs), train_step_loss, linewidth=1, color='blue')
+    plt.plot(np.arange(0,nb_epochs), val_step_loss, linewidth=1, color='red')
+    
+    plt.figure(figsize=(10, 6)) # Set the figure size for better readability
+    plt.plot(np.arange(0,nb_epochs)[1000:], train_step_loss[1000:], linewidth=1, color='blue')
+    plt.plot(np.arange(0,nb_epochs)[1000:], val_step_loss[1000:], linewidth=1, color='red')
+
+    ####rmse training
+    y_train_denormalized = y_pred.detach().numpy()*std_load_train+mean_load_train
+    rmse_train = root_mean_squared_error(Data_train[['Residuals']], y_train_denormalized)
+    print(f'Final Root Mean Squared Error on Train Data: {rmse_train:.4f}')
+
+    
+    ####rmse test
+    # We convert test data to torch tensors
+    variables_to_drop = ['Residuals', 'Load', 'NN', 'Static','Dyn','Em']
+    X1 = Data_test.drop(variables_to_drop, axis=1)
+    X1 = scaler.fit_transform(X1)
+    X1 = X1.astype(np.float32)
+    X1_tensor = torch.from_numpy(X1)
+    
+    y_pred = model(X1_tensor)
+    y_pred_denormalized = y_pred.detach().numpy()*std_load_train+mean_load_train
+    rmse_test = root_mean_squared_error(Data_test[['Residuals']], y_pred_denormalized)
+    print(f'Final Root Mean Squared Error on Test Data: {rmse_test:.4f}')
+
+    #Oracle 
+    y_pred_denormalized_oracle = y_pred.detach().numpy()*std_load_test+mean_load_test
+    rmse_oracle = root_mean_squared_error(Data_test[['Residuals']], y_pred_denormalized_oracle)
+    print(f'Final Root Mean Squared Error on Test Data Oracle: {rmse_oracle:.4f}')
+   
+    ##############Partie Kalman
+    
+    y_mlp = np.concatenate((y_train_denormalized, y_pred_denormalized))
+    y = np.concatenate((Data_train[['Residuals']], Data_test[['Residuals']]))
+    
+    rmse_base = np.sqrt(np.mean(Data_test[['Residuals']]**2))
+    print(f'Residuals on Test Data: {rmse_base:.4f}')
+    
+    
+    #y = y.reshape(-1, 1)  
+    intercept = np.ones(len(y_mlp))
+    intercept = intercept.reshape(-1, 1)  
+    
+    Xkal = np.hstack((intercept,y_mlp.reshape(-1, 1)))
+    
+    ltest = len(Data_test[['Residuals']].values)
+    #### static Kalman 
+    ssm = statespace.StateSpaceModel(Xkal, y, kalman_params= None)
+    mlp_static = ssm.pred_mean
+    rmse_static = root_mean_squared_error(y[-ltest:], mlp_static[-ltest:])
+    print(f'mlp_static Root Mean Squared Error on Test Data: {rmse_static:.4f}')
+     
+    
+    #### dynamic Kalman (grid search)
+    l = iterative_grid_search(Xkal, y, q_list =  2.0 ** np.arange(-60, 1), p1 = 1, ncores=10) 
+    ssm_dyn = statespace.StateSpaceModel(Xkal, y, kalman_params= l)
+    mlp_dyn = ssm_dyn.pred_mean[-ltest:]
+    rmse_dyn = root_mean_squared_error(y[-ltest:], mlp_dyn[-ltest:])
+    print(f'mlp_dyn Root Mean Squared Error on Test Data: {rmse_dyn:.4f}')
+    
+    #### dynamic Kalman (EM)
+    l_em = expectation_maximization(Xkal, y, Q_init = np.eye(Xkal.shape[1]), p1 = 1, n_iter = 3*10^3)  #######1.8s
+    ssm_dyn_em = statespace.StateSpaceModel(Xkal, y, kalman_params= l_em)
+    mlp_dyn_em = ssm_dyn_em.pred_mean[-ltest:]
+    rmse_dyn_em = root_mean_squared_error(y[-ltest:], mlp_dyn_em[-ltest:])
+    print(f'mlp_dyn_em Root Mean Squared Error on Test Data: {rmse_dyn_em:.4f}')
+    
+    
+    indices_to_update = actual_all_hour.between_time(start_time, end_time).index
+    prediction_all_hour_NN.loc[indices_to_update] = y_mlp.flatten()
+    prediction_all_hour_NN_static[indices_to_update] = ssm.pred_mean
+    prediction_all_hour_NN_dyn[indices_to_update] = ssm_dyn.pred_mean
+    prediction_all_hour_NN_em[indices_to_update] = ssm_dyn_em.pred_mean
+
+
+    print(f'Final Root Mean Squared Error on Test Data: {rmse_test:.4f}')
+    print(f'mlp_static Root Mean Squared Error on Test Data: {rmse_static:.4f}')
+    print(f'mlp_dyn Root Mean Squared Error on Test Data: {rmse_dyn:.4f}')
+    print(f'mlp_dyn_em Root Mean Squared Error on Test Data: {rmse_dyn_em:.4f}')
+ 
+
+
+concatenated_pred = pd.concat([actual_all_hour, prediction_all_hour_NN, prediction_all_hour_NN_static, 
+                                prediction_all_hour_NN_dyn, prediction_all_hour_NN_em], axis=1)   
+concatenated_pred.columns = ['Residuals', 'NN', 'Static', 'Dyn', 'Em']
+concatenated_pred.to_csv('/Users/Yannig/Documents/These_Nathan/Mobility_data_review/Results/prediction_MLP_orange_residuals_lag.txt', index=True, header=True)
+
+
+
---
+++ 2-Benchmark/3-Orange estimator/RNN_orange.py
@@ -0,0 +1,337 @@
+# packages import
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons # A simple synthetic dataset
+from sklearn.metrics import root_mean_squared_error
+from sklearn.metrics import mean_squared_error
+from scipy import stats
+
+from viking_kalman import expectation_maximization
+from viking_kalman import iterative_grid_search
+from viking_kalman import statespace
+from viking_kalman import expectation_maximization
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+
+# Device Configuration
+def get_device():
+    if torch.backends.mps.is_available():
+        device = torch.device("mps")
+        print("Using MPS (Metal Performance Shaders) backend on Apple Silicon GPU.")
+    else:
+        device = torch.device("cpu")
+        print("MPS backend not available. Using CPU.")
+    return device
+
+device = get_device()
+
+
+####################################################################################################################################
+# Import Data
+####################################################################################################################################
+
+#National data
+Data = pd.read_csv('Outputs/dataset_national.csv')
+Data['date']= pd.to_datetime(Data['date'])
+Data.set_index("date", inplace=True)
+Data['Time']= np.arange(0, Data.shape[0])
+Data = pd.get_dummies(Data, columns=['day_type_week'], drop_first=True, dtype=int)
+Data = pd.get_dummies(Data, columns=['period_hour_changed'], drop_first=True, dtype=int)
+
+
+covariate_names = Data.columns.tolist()
+covariate_columns = ['Load', 'Load_d1', 'Load_d7', 'temperature',  'temperature_smooth_990',  'temperature_smooth_950', 'temperature_min_smooth_990',
+                     'temperature_max_smooth_990',
+                     'toy', 'day_type_week_1', 'day_type_week_2',
+                     'day_type_week_3', 'day_type_week_4', 'day_type_week_5', 'day_type_week_6', 'period_holiday', 'period_summer', 'period_christmas']
+
+
+Data = Data[covariate_columns]
+last_index = Data.index[-1]
+Data = Data.drop(last_index) 
+
+
+#MLP prev
+Data_prev = pd.read_csv('Results/prediction_MLP_newdate_lag.txt')
+Data_prev = Data_prev.drop(columns=['Load'])
+Data_prev['date']= pd.to_datetime(Data_prev['date'])
+Data_prev.set_index("date", inplace=True)
+Data_prev.index
+
+#Orange Data
+Data_orange = pd.read_csv('Data/data_orange_nathan.csv')
+Data_orange['date']= pd.to_datetime(Data_orange['date'])
+Data_orange = Data_orange[['date', 'Residents_aggl', 'Work', 'Exc_aggl', 'Tourists_aggl']]
+Data_orange.set_index("date", inplace=True)
+Data_orange.index
+Data_orange.columns.tolist()
+
+
+#merging
+merged_df = pd.merge(Data, Data_prev, left_index=True, right_index=True, how='inner')
+merged_df.columns.tolist()
+merged_df.index = merged_df.index.tz_localize(None)
+merged_orange = pd.merge(merged_df, Data_orange, left_index=True, right_index=True, how='inner')
+merged_orange.columns.tolist()
+merged_orange['Residuals'] = merged_orange['Load'] - merged_orange['NN']
+
+
+
+
+####################################################################################################################################
+# Model
+####################################################################################################################################
+covariate_columns = ['Residuals', 'Exc_aggl']
+
+merged_orange.columns.tolist()
+merged_orange = merged_orange[covariate_columns]
+
+
+a = merged_orange[merged_orange.index.astype(str) == "2019-07-08 00:00:00"].index[0]
+actual_all_hour = merged_orange.loc[a:,'Residuals']
+prediction_all_hour_NN = actual_all_hour.copy()
+prediction_all_hour_NN_static = actual_all_hour.copy()
+prediction_all_hour_NN_dyn = actual_all_hour.copy()
+prediction_all_hour_NN_em = actual_all_hour.copy()
+
+
+
+
+def create_sequences(data, sequence_length):
+        X, y = [], []
+        #for i in range(len(data) - sequence_length):
+        for i in range(len(data) - sequence_length):
+
+            # X is the sequence of the last `sequence_length` days
+            X.append(data[i:i + sequence_length])
+            # y is the load of the next day (the target to predict)
+            y.append(data[i + sequence_length, 0])  # Assuming 'Load' is the first column
+        return np.array(X), np.array(y)
+    
+# Set the sequence length (e.g., using the last sequence_length days to predict the next)
+sequence_length = 7
+
+
+
+for j in range(48):
+    hour = j // 2
+    minute = (j % 2) * 30
+    
+    start_time = f'{hour:02d}:{minute:02d}'
+    end_time = f'{hour:02d}:{minute+29:02d}'
+    
+    print('###############################################' + str(j))
+    print('###############################################' + str(start_time)+ '###########'+str(end_time))
+
+    
+    Data_h = merged_orange.between_time(start_time, end_time)
+     
+    d1 = Data_h[Data_h.index.strftime('%Y-%m-%d %H:%M:%S') ==  "2022-09-01 "+ start_time + ":00"].index[0]
+    d2 = Data_h[Data_h.index.strftime('%Y-%m-%d %H:%M:%S') ==  "2022-08-26 "+ start_time + ":00"].index[0]
+
+
+    mean_load_train = np.mean(Data_h.loc[:d1, 'Residuals'])
+    std_load_train = np.std(Data_h.loc[:d1,'Residuals'])
+   
+    mean_load_test = np.mean(Data_h.loc[d2:, 'Residuals'])
+    std_load_test = np.std(Data_h.loc[d2:,'Residuals'])
+   
+    Data_train = Data_h.loc[:d1, ]
+    Data_test = Data_h.loc[d2:, ]
+   
+    scaler = StandardScaler()
+    Data_train_normalized = scaler.fit_transform(Data_train)
+    Data_test_normalized = scaler.fit_transform(Data_test)
+    
+    
+    Data_train_normalized.shape
+    Data_test_normalized.shape
+   
+    # Create the sequences
+    X_train, y_train = create_sequences(Data_train_normalized, sequence_length)
+    X_test, y_test = create_sequences(Data_test_normalized, sequence_length)
+   
+    X_train = torch.Tensor(X_train)
+    y_train = torch.Tensor(y_train).unsqueeze(1)
+   
+    X_test = torch.Tensor(X_test)
+    y_test = torch.Tensor(y_test).unsqueeze(1)
+   
+    # X_train.shape
+    # X_test.shape
+   
+   
+    # Create DataLoader
+    batch_size = 7*10
+    train_loader = DataLoader(TensorDataset(X_train, y_train), shuffle=True, batch_size=batch_size)
+    test_loader = DataLoader(TensorDataset(X_test, y_test), shuffle=False, batch_size=batch_size)
+   
+   
+   
+    class RNNModel(nn.Module):
+           def __init__(self, input_size, hidden_size, output_size):
+               super(RNNModel, self).__init__()
+               self.hidden_size = hidden_size
+               self.rnn = nn.RNN(input_size, hidden_size, batch_first=True)
+               self.fc = nn.Linear(hidden_size, output_size)
+               
+           def forward(self, x):
+               # x shape: (batch_size, sequence_length, input_size)
+               
+               # Initialize hidden state with zeros
+               h0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
+               
+               # Pass through RNN layer
+               out, _ = self.rnn(x, h0)
+               
+               # Get the output from the last time step
+               out = self.fc(out[:, -1, :])
+               return out
+   
+   
+   
+    # Model, loss, and optimizer initialization
+    input_size = X_train.shape[2]  # Number of features: electricity load, temp, humidity, wind_speed
+    hidden_size = 200
+    output_size = 1
+    num_epochs = 100
+    learning_rate = 0.001
+       
+    model = RNNModel(input_size, hidden_size, output_size)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+       
+    # Training loop
+    train_step_loss = []
+     
+    seed = 100
+    if torch.backends.mps.is_available():
+             torch.mps.manual_seed(seed)
+          
+    for epoch in range(num_epochs):
+           for i, (sequences, labels) in enumerate(train_loader):
+               # Forward pass
+               outputs = model(sequences)
+               loss = criterion(outputs, labels)
+               train_step_loss.append(loss.item())
+               
+
+               # Backward and optimize
+               optimizer.zero_grad()
+               loss.backward()
+               optimizer.step()
+           
+           if (epoch + 1) % 10 == 0:
+               print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+        
+   # Evaluation on train set
+    model.eval()
+    train_loader = DataLoader(TensorDataset(X_train, y_train), shuffle=False, batch_size=batch_size)
+    predictions_train = []
+    actuals_train = []
+    with torch.no_grad():
+       for sequences, labels in train_loader:
+           outputs = model(sequences)
+           predictions_train.extend(outputs.squeeze().tolist())
+           actuals_train.extend(labels.squeeze().tolist())
+           
+    actuals_train_denormalized = np.array(actuals_train) * std_load_test + mean_load_test        
+    predictions_train_denormalized = np.array(predictions_train) * std_load_train + mean_load_train
+      
+    rmse_train = np.sqrt(np.mean((predictions_train_denormalized - actuals_train_denormalized)**2))
+    print(f'Final Root Mean Squared Error on Train Data: {rmse_train:.4f}')
+
+   # Evaluation on test set
+    model.eval()
+    predictions = []
+    actuals = []
+    with torch.no_grad():
+       for sequences, labels in test_loader:
+           outputs = model(sequences)
+           outputs_np = outputs.detach().cpu().numpy().flatten()
+           predictions.extend(outputs_np)
+           labels_np = labels.detach().cpu().numpy().flatten()
+           actuals.extend(labels_np)
+   
+   # Denormalize predictions and actuals
+    predictions_denormalized = np.array(predictions) * std_load_train + mean_load_train
+    predictions_denormalized_oracle = np.array(predictions) * std_load_test + mean_load_test
+    actuals_denormalized = np.array(actuals) * std_load_test + mean_load_test
+   
+
+   # Calculate hourly RMSE
+   
+    rmse_base = np.sqrt(np.mean(Data_test[['Residuals']]**2))
+    print(f'Residuals on Test Data: {rmse_base:.4f}')
+    
+    rmse_test = np.sqrt(np.mean((predictions_denormalized - actuals_denormalized)**2))
+    print(f'Final Root Mean Squared Error on Test Data: {rmse_test:.4f}')
+
+    rmse_oracle = np.sqrt(np.mean((predictions_denormalized_oracle - actuals_denormalized)**2))
+    print(f'ORACLE Root Mean Squared Error on Test Data: {rmse_oracle:.4f}')
+
+
+    ##############Kalman
+ 
+    y_rnn = np.concatenate((predictions_train_denormalized, predictions_denormalized))
+    y = np.concatenate((actuals_train_denormalized, actuals_denormalized))
+
+    intercept = np.ones(len(y_rnn))
+    intercept = intercept.reshape(-1, 1)  
+    
+    Xkal = np.hstack((intercept,y_rnn.reshape(-1, 1)))
+
+    #### static Kalman 
+    ssm = statespace.StateSpaceModel(Xkal, y, kalman_params= None)
+    rnn_static = ssm.pred_mean
+    rmse_static = root_mean_squared_error(y[-len(predictions_denormalized):], rnn_static[-len(predictions_denormalized):])
+    print(f'rnn_static Root Mean Squared Error on Test Data: {rmse_static:.4f}')
+
+
+    #### dynamic Kalman (grid search)
+    l = iterative_grid_search(Xkal, y, q_list =  2.0 ** np.arange(-30, 1), p1 = 1, ncores=10) 
+    ssm_dyn = statespace.StateSpaceModel(Xkal, y, kalman_params= l)
+    rnn_dyn = ssm_dyn.pred_mean[-len(predictions_denormalized):]
+    rmse_dyn = root_mean_squared_error(y[-len(predictions_denormalized):], rnn_dyn[-len(predictions_denormalized):])
+    print(f'rnn_dyn Root Mean Squared Error on Test Data: {rmse_dyn:.4f}')
+
+
+    #### dynamic Kalman (EM)
+    l_em = expectation_maximization(Xkal, y, Q_init = np.eye(Xkal.shape[1]), p1 = 1, n_iter = 9*10^3) 
+    ssm_dyn_em = statespace.StateSpaceModel(Xkal, y, kalman_params= l_em)
+    rnn_dyn_em = ssm_dyn_em.pred_mean[-ltest:]
+    rmse_dyn_em = root_mean_squared_error(y[-ltest:], rnn_dyn_em[-ltest:])
+    print(f'rnn_dyn_em Root Mean Squared Error on Test Data: {rmse_dyn_em:.4f}')
+
+    len(indices_to_update)
+    y_rnn.shape
+
+    indices_to_update = actual_all_hour.between_time(start_time, end_time).index
+    prediction_all_hour_NN.loc[indices_to_update] = y_rnn.flatten()
+    prediction_all_hour_NN_static[indices_to_update] = ssm.pred_mean
+    prediction_all_hour_NN_dyn[indices_to_update] = ssm_dyn.pred_mean
+    prediction_all_hour_NN_em[indices_to_update] = ssm_dyn_em.pred_mean
+
+    print(f'Final Root Mean Squared Error on Test Data: {rmse_test:.4f}')
+    print(f'rnn_static Root Mean Squared Error on Test Data: {rmse_static:.4f}')
+    print(f'rnn_dyn Root Mean Squared Error on Test Data: {rmse_dyn:.4f}')
+    print(f'rnn_dyn_em Root Mean Squared Error on Test Data: {rmse_dyn_em:.4f}')
+ 
+
+concatenated_pred = pd.concat([actual_all_hour, prediction_all_hour_NN, prediction_all_hour_NN_static, 
+                                prediction_all_hour_NN_dyn, prediction_all_hour_NN_em], axis=1)   
+concatenated_pred.columns = ['Residuals', 'NN', 'Static', 'Dyn', 'Em']
+concatenated_pred.to_csv('/Users/Yannig/Documents/These_Nathan/Mobility_data_review/Results/prediction_RNN_orange_residuals.txt', index=True, header=True)
+
